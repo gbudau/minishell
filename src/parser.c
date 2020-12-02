0624c13f8028f1941eb37f64df95298a23bfc1dd
gbudau <54190400+gbudau@users.noreply.github.com>
2020-12-02 01:29:50 +0100

    Variable substitution

--- src/parser.c
+++ src/parser.c
@@ -6,14 +6,80 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 15:01:10 by gbudau            #+#    #+#             */
-/*   Updated: 2020/11/27 18:12:44 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/12/02 01:21:52 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int		*expand_words(char *words, t_list **word_list, t_list *environ)
+
+/*
+** The flag variable control if ft_isdigit is used or not
+** This is because POSIX specifies that
+** environmental variables don't begin with a digit
+** https://pubs.opengroup.org/onlinepubs/000095399/basedefs/xbd_chap08.html
+*/
+
+int		is_env_format(char c, int flag)
 {
+	return (ft_isalpha(c) || c == '_' || (flag && ft_isdigit(c)));
+}
+
+char	*env_value(char **words, size_t *i, t_list *environ)
+{
+	size_t	len;
+	char	*env_name;
+
+	len = *i;
+	while (is_env_format((*words)[len], 1))
+		len++;
+	if ((env_name = ft_strndup(&(*words)[*i], len - *i)) == NULL)
+		error_exit();
+	env_name = get_env(environ, env_name);
+	if (env_name == NULL)
+	{
+		env_name = ft_strdup("");
+		if (env_name == NULL)
+			error_exit();
+	}
+	*i += (len - *i) + 1;
+	return (env_name);
+}
+
+void	substitute_env(char **words, size_t *i, t_list *environ)
+{
+	char	*beginning;
+	char	*middle;
+	char	*end;
+
+	*i += 1;
+	if ((*words)[*i] == '\0' || !is_env_format((*words)[*i], 0))
+		return ;
+	if ((beginning = ft_strndup(*words, *i - 1)) == NULL)
+		error_exit();
+	middle = env_value(words, i, environ);
+	if ((end = ft_strndup(&(*words)[*i - 1], ft_strlen(&(*words)[*i - 1]))) == NULL)
+		error_exit();
+	*i = ft_strlen(beginning) + ft_strlen(middle);
+	free(*words);
+	if ((*words = ft_strjoin_free(beginning, middle, FREE_BOTH)) == NULL)
+		error_exit();
+	if ((*words = ft_strjoin_free(*words, end, FREE_BOTH)) == NULL)
+		error_exit();
+}
+
+void	skip_single_quote(char **words, size_t *i)
+{
+	*i += 1;
+	while ((*words)[*i] && (*words)[*i] != '\'')
+		*i += 1;
+	if ((*words)[*i])
+		*i += 1;
+}
+
+int		expand_words(char **words, t_list **word_list, t_list *environ)
+{
+	/*
 	t_token	*token;
 
 	(void)environ;
@@ -27,14 +93,33 @@ int		*expand_words(char *words, t_list **word_list, t_list *environ)
 	*word_list = ft_lstnew(token);
 	if (*word_list == NULL)
 		error_exit();
-	return (0);
+	*/
+	(void)word_list;
+
+	size_t	i;
+
+	i = 0;
+	while ((*words)[i])
+	{
+		if ((*words)[i] == '\'')
+			skip_single_quote(words, &i);
+		else if ((*words)[i] == '\\')
+			i += 2;
+		else if ((*words)[i] == '$')
+		{
+			substitute_env(words, &i, environ);
+		}
+		else if ((*words)[i])
+			i++;
+	}
+	return (1);
 }
 
 void	expansion(t_list **tokens, t_list *environ)
 {
 	t_list	*trav;
 	t_list	*sublist;
-	t_list	*tmp;
+	//t_list	*tmp;
 	t_token	*token;
 
 	while ((trav = *tokens))
@@ -42,13 +127,19 @@ void	expansion(t_list **tokens, t_list *environ)
 		token = trav->content;
 		if (token->type == TOKEN_WORD)
 		{
+			/*
 			tmp = trav->next;
-			expand_words(token->str, &sublist, environ);
-			*tokens = sublist;
-			sublist = ft_lstlast(*tokens);
-			sublist->next = tmp;
-			tokens = &sublist->next;
-			ft_lstdelone(trav, clear_token);
+			if (expand_words(&token->str, &sublist, environ))
+			{
+				*tokens = sublist;
+				sublist = ft_lstlast(*tokens);
+				sublist->next = tmp;
+				tokens = &sublist->next;
+				ft_lstdelone(trav, clear_token);
+			}
+			*/
+			expand_words(&token->str, &sublist, environ);
+			tokens = &trav->next;
 		}
 		else
 			tokens = &trav->next;
