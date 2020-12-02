264a31089542263fe8c2850f81fae90ce93c47fc
gbudau <54190400+gbudau@users.noreply.github.com>
2020-12-02 21:24:54 +0100

    Fixed variable substitution inside double quotes Norminette fixes and improved readability

--- src/parser.c
+++ src/parser.c
@@ -6,12 +6,11 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 15:01:10 by gbudau            #+#    #+#             */
-/*   Updated: 2020/12/02 19:22:37 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/12/02 21:23:10 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
 
 /*
 ** The flag variable control if ft_isdigit is used or not
@@ -31,10 +30,11 @@ char	*env_value(char **words, size_t *i, t_list *environ)
 	char	*env_name;
 	char	*tmp;
 
+	(*i)++;
 	len = *i;
 	while (is_env_format((*words)[len], 1))
 		len++;
-	if ((env_name = ft_strndup(&(*words)[*i], len - *i)) == NULL)
+	if ((env_name = ft_strndup(*words + *i, len - *i)) == NULL)
 		error_exit();
 	tmp = env_name;
 	env_name = get_env(environ, env_name);
@@ -45,23 +45,22 @@ char	*env_value(char **words, size_t *i, t_list *environ)
 		if (env_name == NULL)
 			error_exit();
 	}
-	*i += (len - *i) + 1;
+	*i += (len - *i);
 	return (env_name);
 }
 
-void	substitute_env(char **words, size_t *i, t_list *environ)
+size_t	substitute_env(char **words, size_t *i, t_list *environ)
 {
 	char	*beginning;
 	char	*middle;
 	char	*end;
 
-	*i += 1;
-	if ((*words)[*i] == '\0' || !is_env_format((*words)[*i], 0))
-		return ;
-	if ((beginning = ft_strndup(*words, *i - 1)) == NULL)
+	if ((*words)[*i + 1] == '\0' || !is_env_format((*words)[*i + 1], 0))
+		return (0);
+	if ((beginning = ft_strndup(*words, *i)) == NULL)
 		error_exit();
 	middle = env_value(words, i, environ);
-	if ((end = ft_strndup(&(*words)[*i - 1], ft_strlen(&(*words)[*i - 1]))) == NULL)
+	if ((end = ft_strndup(*words + *i, ft_strlen(*words + *i))) == NULL)
 		error_exit();
 	*i = ft_strlen(beginning) + ft_strlen(middle);
 	free(*words);
@@ -69,60 +68,95 @@ void	substitute_env(char **words, size_t *i, t_list *environ)
 		error_exit();
 	if ((*words = ft_strjoin_free(*words, end, FREE_BOTH)) == NULL)
 		error_exit();
+	return (1);
 }
 
 void	skip_single_quote(char **words, size_t *i)
 {
-	*i += 1;
+	(*i)++;
 	while ((*words)[*i] && (*words)[*i] != '\'')
-		*i += 1;
+		(*i)++;
 	if ((*words)[*i])
-		*i += 1;
+		(*i)++;
 }
+
+size_t	parse_double_quotes(char **words, size_t *i, t_list *environ)
+{
+	size_t	expanded;
+
+	expanded = 0;
+	(*i)++;
+	while ((*words)[*i] != '\0' && (*words)[*i] != '"')
+	{
+		if ((*words)[*i] == '\\')
+			*i += 2;
+		else if ((*words)[*i] == '$')
+			expanded = substitute_env(words, i, environ);
+		else if ((*words)[*i])
+			(*i)++;
+	}
+	return (expanded);
+}
+
+/*
+**	t_token	*token;
+**
+**	(void)environ;
+**	token = malloc(sizeof(*token));
+**	if (token == NULL)
+**		error_exit();
+**	token->type = TOKEN_WORD;
+**	token->str = ft_strdup(words);
+**	if (token->str == NULL)
+**		error_exit();
+**	*word_list = ft_lstnew(token);
+**	if (*word_list == NULL)
+**		error_exit();
+*/
 
 int		expand_words(char **words, t_list **word_list, t_list *environ)
 {
-	/*
-	t_token	*token;
-
-	(void)environ;
-	token = malloc(sizeof(*token));
-	if (token == NULL)
-		error_exit();
-	token->type = TOKEN_WORD;
-	token->str = ft_strdup(words);
-	if (token->str == NULL)
-		error_exit();
-	*word_list = ft_lstnew(token);
-	if (*word_list == NULL)
-		error_exit();
-	*/
-	(void)word_list;
-
+	size_t	expanded;
 	size_t	i;
 
+	(void)word_list;
+	expanded = 0;
 	i = 0;
 	while ((*words)[i])
 	{
 		if ((*words)[i] == '\'')
 			skip_single_quote(words, &i);
+		else if ((*words)[i] == '"')
+			expanded = parse_double_quotes(words, &i, environ);
 		else if ((*words)[i] == '\\')
 			i += 2;
 		else if ((*words)[i] == '$')
-		{
-			substitute_env(words, &i, environ);
-		}
+			expanded = substitute_env(words, &i, environ);
 		else if ((*words)[i])
 			i++;
 	}
-	return (1);
+	return (expanded);
 }
+
+/*
+** //t_list	*tmp;
+**
+**
+** tmp = trav->next;
+** if (expand_words(&token->str, &sublist, environ))
+** {
+** 	*tokens = sublist;
+** 	sublist = ft_lstlast(*tokens);
+** 	sublist->next = tmp;
+** 	tokens = &sublist->next;
+** 	ft_lstdelone(trav, clear_token);
+** }
+*/
 
 void	expansion(t_list **tokens, t_list *environ)
 {
 	t_list	*trav;
 	t_list	*sublist;
-	//t_list	*tmp;
 	t_token	*token;
 
 	while ((trav = *tokens))
@@ -130,17 +164,6 @@ void	expansion(t_list **tokens, t_list *environ)
 		token = trav->content;
 		if (token->type == TOKEN_WORD)
 		{
-			/*
-			tmp = trav->next;
-			if (expand_words(&token->str, &sublist, environ))
-			{
-				*tokens = sublist;
-				sublist = ft_lstlast(*tokens);
-				sublist->next = tmp;
-				tokens = &sublist->next;
-				ft_lstdelone(trav, clear_token);
-			}
-			*/
 			expand_words(&token->str, &sublist, environ);
 			tokens = &trav->next;
 		}
