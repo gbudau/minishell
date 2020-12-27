386d24a9536c801ef494ea0f87c1087a39586f3e
gbudau <54190400+gbudau@users.noreply.github.com>
2020-12-27 15:03:12 +0100

    Fixes echo with multiples -n flag, fixes #11
    Check if there are any -n flag, setting the no_trailing_newline variable
    to TRUE if there are and skipping all the arguments containing that flag

--- src/msh_echo.c
+++ src/msh_echo.c
@@ -6,13 +6,62 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 17:40:39 by gbudau            #+#    #+#             */
-/*   Updated: 2020/12/16 19:03:43 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/12/27 16:00:00 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		msh_echo(t_command *cmd, t_list **environ, int *last_status)
+/*
+** Check if the string is "-n" flag
+** It can containt multiple "n" characters like "-nnn"
+** Return TRUE if it's a "-n" flag or FALSE otherwise
+*/
+
+static int	is_no_trailing_newline_flag(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] != '-' || str[i + 1] != 'n')
+		return (FALSE);
+	i += 2;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+/*
+** Return the numbers of "-n" flags found in the arguments vector
+*/
+
+static int	skip_no_trailing_newline_flags(char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i] && is_no_trailing_newline_flag(argv[i]))
+		i++;
+	return (i);
+}
+
+/*
+** echo builtin command
+**
+** Parameters:
+** cmd = a command structure containing the command arguments count and
+** 		 the command arguments vector
+** environ = a list containing the environmental variable (not used here)
+** last_status = status of the last command (we use this in the exit builtin)
+**
+** Return 0 if it was successfully executed, 1 otherwise.
+*/
+
+int			msh_echo(t_command *cmd, t_list **environ, int *last_status)
 {
 	char	**argv;
 	int		no_trailing_newline;
@@ -21,10 +70,9 @@ int		msh_echo(t_command *cmd, t_list **environ, int *last_status)
 	no_trailing_newline = FALSE;
 	argv = cmd->argv;
 	if (cmd->argc > 1)
-		no_trailing_newline = ft_strcmp(argv[1], "-n") == 0;
+		no_trailing_newline = is_no_trailing_newline_flag(argv[1]);
 	argv++;
-	if (no_trailing_newline)
-		argv++;
+	argv += skip_no_trailing_newline_flags(argv);
 	while (*argv)
 	{
 		ft_putstr_fd(*argv, STDOUT_FILENO);
