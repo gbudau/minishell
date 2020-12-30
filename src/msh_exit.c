9ea0341da3bbed7b16c9649d5da37dbefd908f1e
gbudau <54190400+gbudau@users.noreply.github.com>
2020-12-30 20:23:53 +0100

    feature-exit-builtin
    Work in progress

--- src/msh_exit.c
+++ src/msh_exit.c
@@ -6,16 +6,94 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 17:41:22 by gbudau            #+#    #+#             */
-/*   Updated: 2020/12/16 19:02:39 by gbudau           ###   ########.fr       */
+/*   Updated: 2020/12/30 20:26:12 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../include/minishell.h"
 
-int		msh_exit(t_command *cmd, t_list **environ, int *last_status)
+int			is_mult_overflow(long long *result, long long n,
+		long long sign)
 {
-	(void)cmd;
-	(void)environ;
+	if (sign > 0 && n > (LLONG_MAX / 10))
+		return (TRUE);
+	else if (sign < 0 && n > (LLONG_MIN / 10))
+		return (TRUE);
+	*result = n * 10;
+	return (FALSE);
+}
+
+int			is_add_overflow(long long *result,
+		long long a, long long b, long long sign)
+{
+	if (sign > 0 && a > (LLONG_MAX - b))
+		return (TRUE);
+	else if (sign < 0 && a > (LLONG_MIN - b))
+		return (TRUE);
+	*result = a + b;
+	return (FALSE);
+}
+
+long long	exit_atoll(char *str, int *error)
+{
+	long long	n;
+	long long	mult;
+	long long	sign;
+
+	n = 0;
+	mult = 0;
+	if (*str == '-' && str++)
+		sign = -1;
+	else
+		sign = 1;
+	while (ft_isdigit(*str))
+	{
+		*error = is_mult_overflow(&mult, n, sign);
+		if (*error == TRUE)
+			break ;
+		*error = is_add_overflow(&n, mult, *str - '0', sign);
+		if (*error == TRUE)
+			break ;
+		str++;
+	}
+	if (*str != '\0')
+		*error = TRUE;
+	return ((long long)n * sign);
+}
+
+void		print_numeric_argument_error(char *str, int *last_status)
+{
+	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+	*last_status = 2;
 	exit(*last_status);
-	return (0);
+}
+
+int			msh_exit(t_command *cmd, t_list **environ, int *last_status)
+{
+	int			error;
+	long long	n;
+
+	(void)environ;
+	ft_putstr_fd("exit\n", STDERR_FILENO);
+	if (cmd->argc == 1)
+		exit(*last_status);
+	error = FALSE;
+	n = exit_atoll(cmd->argv[1], &error);
+	if (error == TRUE)
+		print_numeric_argument_error(cmd->argv[1], last_status);
+	if (cmd->argc > 2)
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		*last_status = 1;
+		return (*last_status);
+	}
+	if (n >= 0)
+		n = n % 256;
+	else
+		n = n % 256 + 256;
+	*last_status = (int)n;
+	exit(*last_status);
+	return (*last_status);
 }
