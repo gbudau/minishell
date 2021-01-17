e9be3c53064bc46f074a46493d3fc532da3d4877
gbudau <54190400+gbudau@users.noreply.github.com>
2021-01-17 21:34:22 +0100

    [FIX] Error message for multiple semicolons
    If after a semicolon ";" token there is another semicolon ";" token
    Prints:
    minishell: syntax error near unexpected token `;'
    Fixes #67

--- include/errors.h
+++ include/errors.h
@@ -6,7 +6,7 @@
 /*   By: fportela <fportela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 18:14:05 by gbudau            #+#    #+#             */
-/*   Updated: 2021/01/11 00:56:38 by gbudau           ###   ########.fr       */
+/*   Updated: 2021/01/17 21:20:13 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 void	not_a_valid_identifier(char *str);
 void	error_exit(void);
 int		execve_error(char *str, int error);
-void	error_missing_closing_quote(int error);
+void	error_syntax_scanner(int error);
 void	error_unexpected_token(int error);
 
 #endif
