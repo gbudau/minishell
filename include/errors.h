29e589429dbdceb17d9015e21cf4fe2ab33cf517
gbudau <54190400+gbudau@users.noreply.github.com>
2021-01-18 02:20:39 +0100

    [FIX] Execve error or PATH not set
    Fixes exit status and error messages when execve fails
    Or PATH variable is not set
    Fixes #35

--- include/errors.h
+++ include/errors.h
@@ -6,7 +6,7 @@
 /*   By: fportela <fportela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 18:14:05 by gbudau            #+#    #+#             */
-/*   Updated: 2021/01/11 00:56:38 by gbudau           ###   ########.fr       */
+/*   Updated: 2021/01/18 01:30:04 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 void	not_a_valid_identifier(char *str);
 void	error_exit(void);
-int		execve_error(char *str, int error);
+int		execve_error(t_list *environ, char *str);
 void	error_missing_closing_quote(int error);
 void	error_unexpected_token(int error);
 
