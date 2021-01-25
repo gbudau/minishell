f211b7634291e7beac7dfd6d3db7927881a0363c
gbudau <54190400+gbudau@users.noreply.github.com>
2021-01-25 21:09:15 +0100

    [FIX] Exit status and error message if execve fail
    Fixes a case where dup2 was used with a non existing file descriptor
    
    If a filepath was found and execve failed, use this as an argument for
    execve_error() else will use argv[0]
    
    Removed errno check before using stat(), not needed anymore

--- src/pipeline.c
+++ src/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 17:31:43 by gbudau            #+#    #+#             */
-/*   Updated: 2021/01/17 22:14:18 by gbudau           ###   ########.fr       */
+/*   Updated: 2021/01/25 20:46:38 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ static void	init_pipeline(t_pipeline *p)
 	p->lastpipe[STDOUT_FILENO] = -1;
 }
 
-static void	close_pipe_fds(int *pipefds)
+static void	dup2_close_pipe_fds(int *pipefds, int fd)
 {
+	if (fd != -1 && pipefds[fd] != -1)
+		dup2(pipefds[fd], fd);
 	if (pipefds[STDIN_FILENO] != -1)
 		close(pipefds[STDIN_FILENO]);
 	if (pipefds[STDOUT_FILENO] != -1)
@@ -44,15 +46,9 @@ static void	execute_in_child_process(t_pipeline *p, t_list *environ,
 	if (p->cmd->argv)
 		create_and_set_env(&environ, "_", p->cmd->argv[p->cmd->argc - 1]);
 	if (p->havepipe)
-	{
-		dup2(p->lastpipe[STDIN_FILENO], STDIN_FILENO);
-		close_pipe_fds(p->lastpipe);
-	}
+		dup2_close_pipe_fds(p->lastpipe, STDIN_FILENO);
 	if (p->cmd->ispipe)
-	{
-		dup2(p->curpipe[STDOUT_FILENO], STDOUT_FILENO);
-		close_pipe_fds(p->curpipe);
-	}
+		dup2_close_pipe_fds(p->curpipe, STDOUT_FILENO);
 	if (set_redirections(p->cmd))
 		exit(EXIT_FAILURE);
 	if ((idx = is_builtin(p->cmd)) != -1)
@@ -87,7 +83,7 @@ void		do_pipeline(t_list **commands, t_list *environ, int *last_status)
 			execute_in_child_process(&p, environ, last_status);
 		p.cmd->pid = p.newpid;
 		if (p.havepipe)
-			close_pipe_fds(p.lastpipe);
+			dup2_close_pipe_fds(p.lastpipe, -1);
 		p.havepipe = p.cmd->ispipe;
 		if (p.cmd->ispipe)
 			set_lastpipe_to_curpipe(p.lastpipe, p.curpipe);
