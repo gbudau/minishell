d3e2c4d8290a8aa7fd335cfa44a0914819257f00
gbudau <54190400+gbudau@users.noreply.github.com>
2021-01-27 21:53:33 +0100

    [REFACTOR] Remove a variable from wait_all_childrens
    Removed interrupt_signal variable and using *last_status instead and p->cmd->ispipe
    Nothing else changed, same functionality

--- src/pipeline_utils.c
+++ src/pipeline_utils.c
@@ -6,7 +6,7 @@
 /*   By: gbudau <gbudau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 23:19:57 by gbudau            #+#    #+#             */
-/*   Updated: 2021/01/27 19:16:12 by gbudau           ###   ########.fr       */
+/*   Updated: 2021/01/27 21:48:59 by gbudau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 t_list		*wait_all_childrens(t_pipeline *p, int *last_status)
 {
-	int	interrupt_signal;
-
-	interrupt_signal = 0;
 	p->havepipe = 1;
 	while (p->havepipe)
 	{
 		p->cmd = p->trav->content;
 		waitpid(p->cmd->pid, &p->status, 0);
 		*last_status = get_last_status(p->status);
-		if ((*last_status == 130 || *last_status == 131) && !interrupt_signal)
-			interrupt_signal = *last_status;
+		if (*last_status == 130 || *last_status == 131)
+			print_interrupt_signal(*last_status, p->cmd->ispipe);
 		p->havepipe = p->cmd->ispipe;
 		p->trav = p->trav->next;
 	}
-	print_interrupt_signal(interrupt_signal, 1);
 	return (p->trav);
 }
